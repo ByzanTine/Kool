d151f59ed4758495b9992326afe0c844b385169a
robinbach <bestname@umich.edu>
2015-02-24 14:11:40 -0500

    [Scene][Prefab][Code] +test scene; player1 prefab; userInput/ testPlayCtrl

--- Assets/_Scripts/PlayController.cs
+++ Assets/_Scripts/PlayController.cs
@@ -1,15 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
+
 
 public class PlayController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
