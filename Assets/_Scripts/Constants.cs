97827188f65e4bf56275c7e6b65d07fdc9209882
ByzanTine <morbyzantine@gmail.com>
2015-02-26 04:20:29 -0500

    [Animation] New Character, with CoingStudio's work, only attack animation properly is blended.

--- Assets/_Scripts/Constants.cs
+++ Assets/_Scripts/Constants.cs
@@ -4,6 +4,5 @@ using System.Collections;
 public class Constants : MonoBehaviour {
 
 	// the minimun cool donw time for casting. protect the animation
-	public const float minCastCoolDown = 1.0f;
-	
+	public const float minCastCoolDown = 0.25f;
 }
