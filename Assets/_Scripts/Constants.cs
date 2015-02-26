fca4ef99e633f8557f91e023f5348e28515ea05b
ByzanTine <morbyzantine@gmail.com>
2015-02-26 05:25:01 -0500

    [Animation] Using previous Method to work around the walk-attack animation transition. Now ready to do pressure test.

--- Assets/_Scripts/Constants.cs
+++ Assets/_Scripts/Constants.cs
@@ -4,5 +4,5 @@ using System.Collections;
 public class Constants : MonoBehaviour {
 
 	// the minimun cool donw time for casting. protect the animation
-	public const float minCastCoolDown = 0.25f;
+	public const float minCastCoolDown = 0.8f;
 }
