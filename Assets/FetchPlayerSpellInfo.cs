e021dcdeb3b7f53673d810549a863a21bdb500cb
ByzanTine <morbyzantine@gmail.com>
2015-03-02 04:12:13 -0500

    [Fix] I don't really know what's going on. working on Spell Info UI

---
+++ Assets/FetchPlayerSpellInfo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FetchPlayerSpellInfo : MonoBehaviour {
+
+	private Text text;
+	private string label;
+	private GameObject[] Player;
+	void Start () {
+		text = GetComponent<Text> ();
+		Player = GameObject.FindGameObjectsWithTag (TagList.Player);
+		label = text.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = label;
+		text.text += Player[0].GetComponent<PlayerControl> ().magicID;
+	}
+}
--- Assets/_Scripts/PlayController.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using InControl;
-
-
-public class PlayController : MonoBehaviour {
-
-}
