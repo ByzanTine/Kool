fa42cdfd48ec426cf41f3ba7f79b3b47d91a568d
ByzanTine <morbyzantine@gmail.com>
2015-03-14 21:43:06 -0400

    [Refractor] Health Bar attached to Player and will be compatible with dynamic camera.

--- Assets/_Scripts/PlayerHealthView.cs
+++ Assets/_Scripts/PlayerHealthView.cs
@@ -5,10 +5,21 @@ using UnityEngine.UI;
 public class PlayerHealthView : MonoBehaviour {
 	// use a hot link rather than playerId 
 	public GameObject Player;
+	public int id;
+	private Camera camera;
 	PlayerData pd;
 	BarControl barCon;
+
+	// now for y-axis
+	private float heightOffset = 250.0f; 
+	private float widthOffset = 125.0f;
+	private Vector2 rectOrigin;
+	
 	// Use this for initialization
 	void Start () {
+		// adjust Position.
+		camera = Camera.main;
+		rectOrigin = GetComponent<RectTransform> ().sizeDelta;
 
 		pd = Player.GetComponent<PlayerData> ();
 		if (!pd) {
@@ -22,8 +33,28 @@ public class PlayerHealthView : MonoBehaviour {
 		if (pd) {
 			barCon = GetComponentInChildren<BarControl>();
 			barCon.SetBar(pd.health);
+
+			// get.sizeDelta *= 10.0f / Camera.main.fieldOfView;
+			// get the new transform position
+			Vector3 rawPos = camera.WorldToScreenPoint (Player.transform.position);
+			rawPos.y += heightOffset / camera.fieldOfView;
+			rawPos.x -= widthOffset / camera.fieldOfView;
+
+
+			transform.position = rawPos;
+			GetComponent<RectTransform>().sizeDelta =  rectOrigin * 10.0f/Camera.main.fieldOfView;
 			// Debug.Log(pd.health);
 
 		}
+		else {
+			// TODO fetch the player by id, and get the player data
+
+		}
+
+
 	}
+
+
+
+
 }
