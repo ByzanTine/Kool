263abfe5bc7d620e7f7ca9a16c5034c84a707335
ByzanTine <morbyzantine@gmail.com>
2015-03-22 02:54:57 -0400

    [Item] Fade effect. Struggling

--- Assets/_Scripts/FadeDestroy.cs
+++ Assets/_Scripts/FadeDestroy.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class FadeDestroy : MonoBehaviour {
 	public float livingTime;
+	public float hintTime;
+	private Color startColor;
+	private Color endColor;
+
 	// Use this for initialization
 	void Start () {
-
-
+		startColor = GetComponent<Renderer> ().material.color;
+		endColor = startColor;
+		endColor.a = 0.0f;
 		StartCoroutine (DestroyAndFade ());
 
 	}
@@ -14,11 +19,33 @@ public class FadeDestroy : MonoBehaviour {
 	private IEnumerator DestroyAndFade() {
 
 
-		yield return new WaitForSeconds(livingTime);
+		yield return new WaitForSeconds(livingTime - hintTime);
 		// fade all children
+
+		Debug.Log("Start Fade");
+		for ( int childIndex = 0; childIndex < transform.GetChildCount(); childIndex++)
+		{
+			Transform child = gameObject.transform.GetChild(childIndex);           
+			
+			child.gameObject.AddComponent<FadeDestroy>();
+		}
+		
+		yield return StartCoroutine (LerpColor(startColor, endColor, hintTime));
 
 		Destroy (gameObject);
 	}
-	
 
+	public IEnumerator LerpColor(Color from, Color to, float time) {
+		
+		float i = 0.0f;
+		float rate = 1.0f / time;
+
+		while (i < 1.0f) {
+			i += Time.fixedDeltaTime * rate;
+			GetComponent<Renderer> ().material.color = Color.Lerp(from, to, i);
+			// print(GetComponent<Renderer> ().material.color);
+			// Debug.Log("Lerp once" + i);
+			yield return null;
+		}
+	}
 }
