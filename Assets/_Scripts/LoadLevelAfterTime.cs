5f6fe2356cf4208fc3908b21791ee9776d806f6d
ByzanTine <morbyzantine@gmail.com>
2015-03-30 04:10:09 -0400

    [6b52bba] [Title] Add load level script. Squashed commits: [Start] (+1 squashed commits)

---
+++ Assets/_Scripts/LoadLevelAfterTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelAfterTime : MonoBehaviour {
+	public string levelName;
+	public float waitTime;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (DelayedLoadLevel (waitTime));
+	}
+	IEnumerator DelayedLoadLevel(float waitTime_) {
+		yield return new WaitForSeconds(waitTime_);
+		Application.LoadLevel (levelName);
+	}
+
+}
