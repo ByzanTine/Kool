380ef5943f87d7563de9dcacdbeef36549f6cdd8
lslsheng <527873752@qq.com>
2015-03-30 16:31:03 -0400

    merge dev

---
+++ Assets/specialSpellView.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+[RequireComponent(typeof(Text))]
+
+public class specialSpellView : MonoBehaviour {
+	
+	public int playerId;
+	private Camera viewCamera;
+	PlayerData pd;
+	BarControl barCon;
+	GameObject Player;
+	public Sprite Iceburst;
+	public Sprite Meteor;
+	public Sprite Reflect;
+
+	// now for y-axis
+	private float heightOffset = 435.0f; 
+	private float widthOffset = 25f;
+	private Vector2 rectOrigin;
+	private Image img;
+
+	
+	// Use this for initialization
+	void Start () {
+		// adjust Position.
+		viewCamera = Camera.main;
+		rectOrigin = GetComponent<RectTransform> ().sizeDelta;
+		img = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (pd) {
+			switch (pd.SpecialSpellID)
+			{
+				case SpellDB.AttackID.iceBurst:
+					img.sprite = Iceburst;
+					img.enabled = true;
+					break;
+				case SpellDB.AttackID.meteor:
+					img.sprite = Meteor;
+					img.enabled = true;	
+					break;
+				case SpellDB.AttackID.reflect:
+					img.sprite = Reflect;
+					img.enabled = true;
+					break;
+				default:
+					print ("Not a special Spell");
+					img.enabled = false;
+					break;
+			}
+
+
+			Vector3 rawPos = viewCamera.WorldToScreenPoint (Player.transform.position);
+			rawPos.y += heightOffset / viewCamera.fieldOfView;
+			rawPos.x -= widthOffset / viewCamera.fieldOfView;
+			
+			
+			transform.position = rawPos;
+			GetComponent<RectTransform>().sizeDelta =  rectOrigin * 10.0f/viewCamera.fieldOfView;
+			// Debug.Log(pd.health);
+			
+		}
+		else {
+			// fetch the player by id, and get the player data
+			img.enabled = false;
+			Player = GameStatus.GetPlayerObjById(playerId);
+			if(Player)
+				pd = Player.GetComponent<PlayerData> ();
+		}
+	}
+
+	
+	
+}
