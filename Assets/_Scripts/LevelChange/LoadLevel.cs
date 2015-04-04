b94c352cf0237503098e75a51a3a51ff76ffc153
Byzantine <morbyzantine@gmail.com>
2015-04-03 23:36:43 -0400

    [Fix] Clean the folder a bit.

--- Assets/_Scripts/LevelChange/LoadLevel.cs
+++ Assets/_Scripts/LevelChange/LoadLevel.cs
@@ -2,17 +2,8 @@
 using System.Collections;
 
 public class LoadLevel : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
+		
+		
 	public void LoadLevelWithName(string levelName)
 	{
 		Application.LoadLevel (levelName);
