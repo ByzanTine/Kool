9966aa6f976a3a9497098d983f3651631ec96378
lslsheng <527873752@qq.com>
2015-04-15 21:45:21 -0400

    new score board without pic done

---
+++ Assets/_Scripts/AddCross.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AddCross : MonoBehaviour {
+	public float scale = 2.5f;
+	public float shrinkSpeed = 1.2f;
+	private Vector3 initScale; 
+	private bool adding;
+	// Use this for initialization
+	void Start () {
+		initScale = transform.localScale;
+		transform.localScale *= scale;
+		gameObject.GetComponent<Image> ().enabled = false;
+	}
+
+	void Update (){
+		if (initScale.x < transform.localScale.x && adding)
+			transform.localScale -= Vector3.one*Time.deltaTime*shrinkSpeed;
+	}
+	
+
+	public void AddingCross(){
+		adding = true;
+		gameObject.GetComponent<Image> ().enabled = true;
+	}
+}
