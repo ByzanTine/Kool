c79d02d1074bc1344cc50e65883fbc8cf01a41a3
Jingkui Wang <wjkcow@gmail.com>
2015-04-17 23:26:38 -0400

    add credit scene, MainMapAddWall: change crystall larger to block

---
+++ Assets/_Scripts/CreditScrollUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditScrollUp : MonoBehaviour {
+	private float speed = 50f;
+	private float maxY = 600;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < maxY) {
+			transform.Translate (Vector3.up * Time.deltaTime * speed);
+
+		} else {
+			CreditEnd();
+		}
+	}	
+	void CreditEnd(){
+		print ("End");
+	}
+}
