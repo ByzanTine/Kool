eba691ff22ae22eb6750612ac122813c4206b4e9
ByzanTine <morbyzantine@gmail.com>
2015-04-19 21:40:00 -0400

    Trailer stuff

---
+++ Assets/CameraLookAt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLookAt : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (new Vector3(0,0,0), Vector3.up, 0.5f);
+	}
+}
