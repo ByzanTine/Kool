f214b6deef17a06cbfd9eb55241743b6b6ce1500
ByzanTine <morbyzantine@gmail.com>
2015-04-23 04:58:03 -0400

    [UI] Use a better lerp for cross

--- Assets/_Scripts/AddCross.cs
+++ Assets/_Scripts/AddCross.cs
@@ -14,14 +14,26 @@ public class AddCross : MonoBehaviour {
 		gameObject.GetComponent<Image> ().enabled = false;
 	}
 
-	void Update (){
-		if (initScale.x < transform.localScale.x && adding)
-			transform.localScale -= Vector3.one*Time.deltaTime*shrinkSpeed;
-	}
+//	void Update (){
+//		if (initScale.x < transform.localScale.x && adding)
+//			transform.localScale -= Vector3.one*Time.deltaTime*shrinkSpeed;
+//	}
 	
 
 	public void AddingCross(){
 		adding = true;
 		gameObject.GetComponent<Image> ().enabled = true;
+		StartCoroutine (ScaleLerp (transform.localScale, initScale, 1.0f));
+	}
+
+	IEnumerator ScaleLerp(Vector3 from, Vector3 to, float time) {
+		float i = 0.0f;
+		float rate = 1.0f / time;
+		while (i < 1.0f) {
+			i += Time.fixedDeltaTime * rate;
+			transform.localScale = from + (to - from) * Mathf.Pow(i, 5);
+			// Debug.Log("Lerp once" + i);
+			yield return null;
+		}
 	}
 }
